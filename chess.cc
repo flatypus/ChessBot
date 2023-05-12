47f746fbe92e5c9e32fe608df322c0d85d6e0686
flatypus[L] <yhc3141@gmail.com>
2023-05-12 10:52:45 -0700

    can move pawn and take piece

--- chess.cc
+++ chess.cc
@@ -3,208 +3,304 @@
 #include <string>
 #include <cstdio>
 #include <cstring>
+#include <map>
+#include "color.h"
+#include "utils.h"
 
 using namespace std;
 
-std::vector<char> coords = {'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
+vector<char> coords = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+map<char, int> letter_map = {
+    {'a', 0},
+    {'b', 1},
+    {'c', 2},
+    {'d', 3},
+    {'e', 4},
+    {'f', 5},
+    {'g', 6},
+    {'h', 7}};
+map<char, int> num_map = {
+    {'1', 0},
+    {'2', 1},
+    {'3', 2},
+    {'4', 3},
+    {'5', 4},
+    {'6', 5},
+    {'7', 6},
+    {'8', 7}};
 
 class Piece
 {
+private:
+    int color;
+    Symbol symbol;
+    int x;
+    int y;
+
 public:
-    const static int BLACK = 0;
-    const static int WHITE = 1;
-
+    const static int WHITE = 0;
+    const static int BLACK = 1;
+    const static int EMPTY = -1;
     Piece() {}
-    Piece(char symbol) : symbol(symbol) {}
-    Piece(char color, int symbol) : symbol(symbol), color(color) {}
-    Piece(char color, int symbol, int x, int y) : symbol(symbol), color(color), x(x), y(y) {}
-
-    int getColor() const
-    {
-        return color;
-    }
-    char getSymbol() const
+    Piece(int color, Symbol symbol, int x, int y) : color(color),
+                                                    symbol(symbol),
+                                                    x(x),
+                                                    y(y) {}
+    Symbol getSymbol()
     {
         return symbol;
     }
-    int getX() const
+    int getColor()
+    {
+        return color;
+    }
+    int getX()
     {
         return x;
     }
-    int getY() const
+    int getY()
     {
         return y;
     }
-
-    void setColor(int color)
+    virtual bool validMove(int x, int y)
     {
-        this->color = color;
+        return true;
     }
-    void setSymbol(char symbol)
+    // when cout
+    void getInfo()
     {
-        this->symbol = symbol;
+        cout << "Piece: " << (char)(symbol) << " " << color << " " << x << " " << y << endl;
     }
-    void setX(int x)
-    {
-        this->x = x;
-    }
-    void setY(int y)
-    {
-        this->y = y;
-    }
-
-private:
-    int color;
-    int x;
-    int y;
-    char symbol;
 };
 
-class ChessBoard
+class Pawn : public Piece
 {
 public:
-    ChessBoard()
+    Pawn(int color, int x, int y) : Piece(color, Symbol::PAWN, x, y) {}
+    bool validMove(int x, int y)
     {
-        const int SIZE = 8;
-        const vector<Piece> FIRST_RANK{
-            Piece(Piece::BLACK, 'R', 0, 0), Piece(Piece::BLACK, 'N', 1, 0), Piece(Piece::BLACK, 'B', 2, 0),
-            Piece(Piece::BLACK, 'Q', 3, 0), Piece(Piece::BLACK, 'K', 4, 0), Piece(Piece::BLACK, 'B', 5, 0),
-            Piece(Piece::BLACK, 'N', 6, 0), Piece(Piece::BLACK, 'R', 7, 0)};
-        vector<Piece> SECOND_RANK(SIZE, Piece(Piece::BLACK, 'P', 0, 1));
-
-        for (int x = 0; x < SIZE; x++)
+        cout << this->getX() << " " << this->getY() << " " << x << " " << y << endl;
+        getInfo();
+        bool valid = false;
+        switch (this->getColor())
         {
-            SECOND_RANK[x].setX(x);
+        case Piece::WHITE:
+            valid = (this->getY() == 1 && y == 3 || this->getY() + 1 == y) && this->getX() == x;
+            break;
+        case Piece::BLACK:
+            valid = (this->getY() == 6 && y == 4 || this->getY() - 1 == y) && this->getX() == x;
+            break;
         }
+        cout << valid << endl;
+        return valid;
+    };
+};
 
-        board.resize(SIZE, vector<Piece>(SIZE, Piece()));
-        board[0] = FIRST_RANK;
-        board[1] = SECOND_RANK;
-        for (int i = 2; i < SIZE - 2; ++i)
-        {
-            board[i] = vector<Piece>(SIZE, Piece('.'));
-        }
-        vector<Piece> SEVENTH_RANK(SIZE, Piece(Piece::WHITE, 'P', 0, 6));
-
-        for (int x = 0; x < SIZE; x++)
-        {
-            SEVENTH_RANK[x].setX(x);
-        }
-
-        board[SIZE - 2] = SEVENTH_RANK;
-        const vector<Piece> EIGHTH_RANK{
-            Piece(Piece::WHITE, 'R', 0, 7), Piece(Piece::WHITE, 'N', 1, 7), Piece(Piece::WHITE, 'B', 2, 7),
-            Piece(Piece::WHITE, 'Q', 3, 7), Piece(Piece::WHITE, 'K', 4, 7), Piece(Piece::WHITE, 'B', 5, 7),
-            Piece(Piece::WHITE, 'N', 6, 7), Piece(Piece::WHITE, 'R', 7, 7)};
-        board[SIZE - 1] = EIGHTH_RANK;
+class Rook : public Piece
+{
+public:
+    Rook(int color, int x, int y) : Piece(color, Symbol::ROOK, x, y) {}
+    bool validMove(int x, int y)
+    {
+        return false;
     }
+};
 
-    const vector<vector<Piece>> &getBoard() const
+class Knight : public Piece
+{
+public:
+    Knight(int color, int x, int y) : Piece(color, Symbol::KNIGHT, x, y) {}
+    bool validMove(int x, int y)
     {
-        return board;
+        return false;
     }
+};
 
-    void setPiece(int x, int y, Piece piece)
+class Bishop : public Piece
+{
+public:
+    Bishop(int color, int x, int y) : Piece(color, Symbol::BISHOP, x, y) {}
+    bool validMove(int x, int y)
     {
-        board[piece.getY()][piece.getX()].setSymbol('.');
-        board[x][y] = piece;
+        return false;
+    }
+};
+
+class Queen : public Piece
+{
+public:
+    Queen(int color, int x, int y) : Piece(color, Symbol::QUEEN, x, y) {}
+    bool validMove(int x, int y)
+    {
+        return false;
+    }
+};
+
+class King : public Piece
+{
+public:
+    King(int color, int x, int y) : Piece(color, Symbol::KING, x, y) {}
+    bool validMove(int x, int y)
+    {
+        return false;
+    }
+};
+
+class Board
+{
+    mutable vector<vector<Piece *>> board;
+    mutable int turn = Piece::WHITE;
+
+public:
+    Board()
+    {
+        setupBoard();
+        while (true)
+        {
+            displayBoard();
+            requestMove();
+            switchTurn();
+        }
     }
 
 private:
-    vector<vector<Piece>> board;
-};
-
-void printBoard(const ChessBoard &board)
-{
-    const auto &b = board.getBoard();
-    for (const auto &row : b)
+    void switchTurn()
     {
-        for (const auto &piece : row)
+        turn = turn == Piece::WHITE ? Piece::BLACK : Piece::WHITE;
+    }
+    bool inRange(char file, char rank)
+    {
+        return !(letter_map.find(file) == letter_map.end() || num_map.find(rank) == num_map.end());
+    }
+    void requestMove()
+    {
+        string input;
+        cout << "It is " << (turn == Piece::WHITE ? "white" : "black") << "'s turn" << endl;
+        cout << "Enter a move: " << endl;
+        cin >> input;
+        if (!parseMove(input))
         {
-            cout << piece.getSymbol() << " ";
+            displayBoard();
+            requestMove();
         }
-        cout << endl;
     }
-}
 
-bool checkMove(int startX, int startY, int endX, int endY, char symbol)
-{
-    if (startX == endX && startY == endY)
+    Piece *pieceFromSymbol(Symbol symbol, int color, int x, int y)
     {
-        cout << "Cannot move to same location";
-        return false;
-    }
-    cout << startX << " " << startY << " " << endX << " " << endY << " " << symbol << endl;
-    switch (symbol)
-    {
-    case 'K':
-        if (abs(startX - endX) > 1 || abs(startY - endY) > 1)
+        switch (symbol)
         {
-            cout << "Invalid move for King";
+        case Symbol::PAWN:
+            return new Pawn(color, x, y);
+        case Symbol::ROOK:
+            return new Rook(color, x, y);
+        case Symbol::KNIGHT:
+            return new Knight(color, x, y);
+        case Symbol::BISHOP:
+            return new Bishop(color, x, y);
+        case Symbol::QUEEN:
+            return new Queen(color, x, y);
+        case Symbol::KING:
+            return new King(color, x, y);
+        default:
+            return new Piece(Piece::EMPTY, Symbol::EMPTY, x, y);
+        }
+    }
+
+    void makeMove(Piece *piece, int new_file, int new_rank)
+    {
+        cout << new_file << " " << new_rank << endl;
+        int old_file = piece->getX();
+        int old_rank = piece->getY();
+        board[old_rank][old_file] = new Piece(Piece::EMPTY, Symbol::EMPTY, old_file, old_rank);
+        board[new_rank][new_file] = piece;
+    }
+
+    bool parseMove(string input)
+    {
+        if (input.length() < 2)
+        {
+            cout << "Invalid move" << endl;
             return false;
         }
-    }
-    return true;
-}
-
-void doMove(const string &move, ChessBoard &board, int moveNumber)
-{ // rn only works for three letter things like ke5...
-    // cout<<move[2]<<endl;
-    char m = move[0];
-    // Piece moved(moveNumber%2, toupper(m));
-    Piece moved;
-    const auto &b = board.getBoard();
-    for (const auto &row : b)
-    {
-        for (const auto &piece : row)
+        char piece = input.length() == 2 ? 'P' : input[0];
+        char rank = input[input.length() - 1];
+        char file = input[input.length() - 2];
+        vector<Piece *> valid_pieces = {};
+        for (vector<Piece *> row : board)
         {
-            if (piece.getSymbol() == toupper(m) && piece.getColor() == moveNumber % 2)
+            for (Piece *p : row)
             {
-                moved = piece;
-            }
-        }
-    }
-    char horizontalCoord = move[1];
-    int verticalCoord = move[2] - '0';
-    int verticalInt = 0;
-
-    for (int y = 1; y < 8; y++)
-    {
-        if (y == verticalCoord)
-        {
-            for (vector<char>::iterator t = coords.begin(); t != coords.end(); ++t)
-            {
-                if (*t == horizontalCoord)
+                if ((char)(p->getSymbol()) == piece && p->getColor() == turn)
                 {
-                    cout << horizontalCoord - 1 << ", " << verticalInt << endl;
-                    cout << moved.getX() << ", " << moved.getY() << endl;
-                    if (checkMove(moved.getX(), moved.getY(), horizontalCoord, verticalInt, moved.getSymbol()))
-                    {
-                        board.setPiece(verticalInt, horizontalCoord - 1, moved);
-                    }
-                    break;
+                    valid_pieces.push_back(p);
                 }
             }
         }
-        verticalInt++;
+        for (Piece *p : valid_pieces)
+        {
+            int x = letter_map[file];
+            int y = num_map[rank];
+            if (p->getX() == x && p->getY() == y)
+                continue;
+            if (p->validMove(letter_map[file], num_map[rank]))
+            {
+                cout << "valid move" << endl;
+                makeMove(p, x, y);
+                return true;
+            }
+        }
+        cout << "Invalid move" << endl;
+        return false;
     }
-}
+
+    void setupBoard()
+    {
+        vector<Symbol> backRow = {Symbol::ROOK, Symbol::KNIGHT, Symbol::BISHOP, Symbol::QUEEN, Symbol::KING, Symbol::BISHOP, Symbol::KNIGHT, Symbol::ROOK};
+        board.resize(8);
+        for (int i = 0; i < 8; i++)
+        {
+            board[0].push_back(pieceFromSymbol(backRow[i], Piece::WHITE, i, 0));
+
+            board[1].push_back(pieceFromSymbol(Symbol::PAWN, Piece::WHITE, i, 1));
+            for (int j = 2; j < 6; j++)
+            {
+                board[j].push_back(pieceFromSymbol(Symbol::EMPTY, Piece::EMPTY, i, j));
+            }
+            board[6].push_back(pieceFromSymbol(Symbol::PAWN, Piece::BLACK, i, 6));
+            board[7].push_back(pieceFromSymbol(backRow[i], Piece::BLACK, i, 7));
+        }
+    }
+    void displayBoard()
+    {
+        for (int y = board.size() - 1; y >= 0; y--)
+        {
+            cout << y << " ";
+            cout << y + 1 << " ";
+            for (int x = 0; x < board[0].size(); x++)
+            {
+                int color = board[y][x]->getColor();
+                Symbol symbol = board[y][x]->getSymbol();
+                cout << Color::getColoredString(symbol, Foreground::WHITE, (x + y) % 2 == 0 ? Background::BEIGE : Background::GREEN, color);
+            }
+            cout << endl;
+        }
+        cout << "    ";
+        for (int i = 0; i < 8; i++)
+        {
+            cout << " " << coords[i] << " ";
+        }
+        cout << endl
+             << "    ";
+        for (int i = 0; i < 8; i++)
+        {
+            cout << " " << i << " ";
+        }
+        cout << endl;
+    }
+};
 
 int main()
 {
-    std::string move;
-    int moveNumber = 1;
-    ChessBoard board;
-    printBoard(board);
-
-    while (true)
-    {
-        std::cout << "Enter move: ";
-        std::cin >> move;
-        // if(moveNumber%2 == 0)
-        doMove(move, board, moveNumber);
-        printBoard(board);
-        moveNumber++;
-    }
+    Board board = Board();
     return 0;
 }
